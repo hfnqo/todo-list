04c0e59e00db9964af21921de24fc77d9a5466cd
hfnqo <joushitsu22@gmail.com>
2023-02-18 09:55:52 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const { engine } = require('express-handlebars')
+const bodyParser = require('body-parser')  // 引用 body-parser
+
 const Todo = require('./models/todo')  // 載入 Todo model
 
 const app = express()
@@ -29,11 +31,26 @@ db.once('open', () => {
 app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   Todo.find()  // 取出 Todo model 裡的所有資料
     .lean()    // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 模板
     .catch(error => console.error(error))  // 錯誤處理
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  
+  return Todo.create({ name })
+    .then(() => { res.redirect('/') })
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {

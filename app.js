4cd1d5a6ff430abe543c62659f2032be8019e1e2
hfnqo <joushitsu22@gmail.com>
2023-02-27 10:27:22 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find()  // 取出 Todo model 裡的所有資料
     .lean()    // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({ _id: 'asc' })  // 升冪排序 asc, 降冪排序 desc
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 模板
     .catch(error => console.error(error))  // 錯誤處理
 })

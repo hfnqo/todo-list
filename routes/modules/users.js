808346020361f22119f6766179f6f35df328a18b
hfnqo <joushitsu22@gmail.com>
2023-04-21 11:48:23 +0800

    feat: add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,12 +5,13 @@ const passport = require('passport')
 const User = require('../../models/user')
 
 router.get('/login',  (req, res) => {
-  res.render('login') 
+  res.render('login')
 })
 
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
-  failureRedirect: '/users/login'
+  failureRedirect: '/users/login',
+  failureFlash: true
 }))
 
 router.get('/register', (req, res) => {
@@ -19,20 +20,31 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
 
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register',  {
+      errors, name, email, password, confirmPassword
+    })
+  }
   User.findOne({ email }).then(user => {
       if (user) {
-        console.log('User already exists.')
+        errors.push({ message: '這個 Email 已經註冊過了。' })
         res.render('register', {
-          name, email, password, confirmPassword
+          errors, name, email, password, confirmPassword
         })
-      } else {
-        return User.create({ 
-          name, email, password 
-        })
+      }
+      return User.create({ 
+        name, email, password 
+      })
         .then(() => res.redirect('/'))
         .catch(err => console.log(err))
-      }
     })
     .catch(err => console.log(err))
 })
@@ -40,6 +52,7 @@ router.post('/register', (req, res) => {
 router.get('/logout', (req, res, next) => {
   req.logout(err => {
     if (err) return next(err)
+    req.flash('success_msg', '你已經成功登出。')
     res.redirect('/users/login')
   })
 })
